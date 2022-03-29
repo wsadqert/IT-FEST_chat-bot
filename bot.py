386ff2b4bbd8e6b9b0261645182c360455c4b614
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-29 17:59:21 +0300

    added colors, string constants, `requirements.txt`

--- bot.py
+++ bot.py
@@ -1,14 +1,28 @@
-import telebot
+from telebot import TeleBot, types
+from telebot.types import KeyboardButton
 from typing import Final
 
-info: str = """"""
+info: str = """*здесь будет инфа про скиллы моего бота*"""
 token: Final[str] = "5285755435:AAGkUYDMlugF5J0ksNxBB20ZxNbtnLBs_eY"
-bot = telebot.TeleBot(token)
+bot: TeleBot = TeleBot(token)
+
+contacts: Final[str] = """Привет! Если у вас возникли какие-либо вопросы, то вот наши контакты:
+Группа ВКонтакте Научим.online https://vk.com/nauchim.online
+Сайт с мероприятиями https://www.научим.online"""
 
 
 @bot.message_handler(commands=['start'])
 def start(m, res=False):
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	markup.add(KeyboardButton("Факт"))
+	markup.add(KeyboardButton("Наши контакты"))
+
 	bot.send_message(m.chat.id, "Привет! Я на связи! Вот что я умею:")
 	bot.send_message(m.chat.id, info)
 	bot.send_message(m.chat.id, "Для управления используй кнопки 👇")
 
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+	if message.text.strip() == 'Наши контакты':
+		bot.send_message(message.chat.id, contacts)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from colorama import Fore
 from bot import *
 from vk_parser import *
 from database import *
+
+GREEN: str = Fore.GREEN
+RED: str = Fore.RED
+RESET: str = Fore.RESET
+
+print(f'{GREEN}[+] bot started!')
+bot.polling(none_stop=True, interval=0)
+
+print(f'{RED}[*] bot stopped!')

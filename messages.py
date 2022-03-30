a2ea3dcd3ad1e01b26b54e61318ac7a9df16e0cd
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-30 17:18:23 +0300

    added `messages.py`, buttons

---
+++ messages.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+info: Final[str] = """*здесь будет инфа про скиллы моего бота*"""
+contacts: Final[str] = """Привет! Если у вас возникли какие-либо вопросы, то вот наши контакты:
+Группа ВКонтакте Научим.online https://vk.com/nauchim.online
+Сайт с мероприятиями https://www.научим.online"""

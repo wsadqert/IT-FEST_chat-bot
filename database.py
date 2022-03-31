b6c40e702b45d16493c44cf245f8bd01ada22276
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-03-31 10:32:45 +0300

    fixed `database.py`

--- database.py
+++ database.py
@@ -4,12 +4,12 @@ cur = con.cursor()
 
 try:
 	# Create table
-	cur.execute('''CREATE TABLE data (int user_id, boolean TechnoCom, boolean IT-fest_2022, boolean IASF2022, boolean ФестивальОКК, boolean Нейрофест, boolean НевидимыйМир, boolean КонкурсНИР, boolean VRARFest3D)''')
+	cur.execute('''CREATE TABLE data (user_id int, TechnoCom boolean, IT_fest_2022 boolean, IASF2022 boolean, ФестивальОКК boolean, Нейрофест boolean, НевидимыйМир boolean, КонкурсНИР boolean, VRARFest3D boolean)''')
 except sqlite3.OperationalError:
-	pass
+	raise
 
 # Insert a row of data
-cur.execute("INSERT INTO data VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+# cur.execute("INSERT INTO data VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
 # Save (commit) the changes
 con.commit()

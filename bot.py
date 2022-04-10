43d1aa243286b80591e159e81831c8a2d4d0cd52
PM-95025 <50608884+PM-95025@users.noreply.github.com>
2022-04-10 12:16:15 +0300

    if -> match, renamed `hashtags` to `hashtags_enabled`, added `send_help()`, changed `buttons`

--- bot.py
+++ bot.py
@@ -10,26 +10,29 @@ dp = Dispatcher(bot)
 
 # Create main menu markup
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-buttons1: tuple[str, ...] = ("Подписаться", "Отписаться")
-buttons2: tuple[str, ...] = ("Список подписок", 'Наши контакты')
+buttons1: tuple[str, ...] = ("Подписаться", "Отписаться", "Список подписок")
+buttons2: tuple[str, ...] = ("Помощь", "Контакты для связи")
 markup.add(*buttons1)
 markup.add(*buttons2)
 
 markup_help = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup_help.add(['Помощь'])
 
-help_img = InputFile(HELP_IMG_PATH)
-
 empty_markup = types.ReplyKeyboardRemove
 
 section: str = 'main'
 
 
+@dp.callback_query_handler(text='help')
+async def send_help(message):
+	await bot.send_photo(message.from_user.id, InputFile(HELP_IMG_PATH))
+
+
 # Start/help
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def start(message):
 	if section == 'main':
-		await bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}! Для управления используй кнопки \U0001f447", reply_markup=markup)
+		await bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}! Для управления используй кнопки \u0001f447", reply_markup=markup)
 		db.init_user(message.from_user.id)
 	else:
 		await bot.send_message(message.from_user.id, "Ошибка: бот уже запущен!")
@@ -56,50 +59,42 @@ async def answer{HASHTAGS.index(hashtag)}(call: types.CallbackQuery):
 			raise Exception('unknown section "{section}"')
 		db.cur.execute(f"UPDATE data SET {hashtag[1:]} = true WHERE user_id = {message.from_user.id}")
 	except Exception as e:
-		await call.bot.send_message({message.from_user.id}, f'Произошла неизвестная ошибка!\u274c\U0001f937')
+		await call.bot.send_message({message.from_user.id}, f'Произошла неизвестная ошибка!\u274c\u0001f937')
 		await call.bot.send_message({message.from_user.id}, str(e))
 	else:
-		await call.bot.send_message({message.from_user.id}, f'Поздравляю!\U0001f389\U0001f38a Ты успешно подписался на обновления группы VK {GROUP_IDS[HASHTAGS.index(hashtag)]} по хэштегу {hashtag}')
+		await call.bot.send_message({message.from_user.id}, f'Поздравляю!\u0001f389\u0001f38a Ты успешно подписался на обновления группы VK {GROUP_IDS[HASHTAGS.index(hashtag)]} по хештегу {hashtag}')
 """)
-
-	@dp.callback_query_handler(text='help')
-	async def answer_help(call: types.CallbackQuery):
-		await bot.send_photo(message.from_user.id, help_img)
 
 
 async def unsubscribe(message):
 	global section
 	section = 'unsubscribe'
 
-	hashtags = list(compress(HASHTAGS, await _subscriptions(message)))
+	hashtags_enabled = list(compress(HASHTAGS, await _subscriptions(message)))
 
 	# Создание markup`а
 	markup_hashtags = types.InlineKeyboardMarkup()
-	for hashtag in hashtags:
+	for hashtag in hashtags_enabled:
 		markup_hashtags.add(types.InlineKeyboardButton(text=hashtag, callback_data=hashtag[1:]))
 	markup_hashtags.add(types.InlineKeyboardButton(text='Помощь', callback_data='help'))
 
 	await bot.send_message(message.from_user.id, UNSUBSCRIBE_TEXT, reply_markup=markup_hashtags)
 
 	# создание и привязка функций к кнопкам
-	for hashtag in hashtags:
+	for hashtag in hashtags_enabled:
 		exec(f"""
 @dp.callback_query_handler(text=hashtag[1:])
-async def answer{hashtags.index(hashtag) + 10}(call: types.CallbackQuery):
+async def answer{hashtags_enabled.index(hashtag) + 10}(call: types.CallbackQuery):
 	try:
 		if '{section}' != 'unsubscribe':
 			raise Exception('unknown section "{section}"')
 			db.cur.execute(f"UPDATE data SET {hashtag[1:]} = false WHERE user_id = {message.from_user.id}")
 	except Exception as e:
-		await call.bot.send_message({message.from_user.id}, f'Произошла неизвестная ошибка!\u274c\U0001f937')
+		await call.bot.send_message({message.from_user.id}, f'Произошла неизвестная ошибка!\u274c\u0001f937')
 		await call.bot.send_message({message.from_user.id}, str(e))
 	else:
-		await call.bot.send_message({message.from_user.id}, f'Ты успешно отписался от обновлений группы VK {GROUP_IDS[HASHTAGS.index(hashtag)]} по хэштегу {hashtag}. Если это сделано случайно, ты всегда можешь возобновить подписку.')
+		await call.bot.send_message({message.from_user.id}, f'Ты успешно отписался от обновлений группы VK {GROUP_IDS[HASHTAGS.index(hashtag)]} по хештегу {hashtag}. Если это сделано случайно, ты всегда можешь возобновить подписку.')
 """)
-
-	@dp.callback_query_handler(text='help')
-	async def answer_help(call: types.CallbackQuery):
-		await bot.send_photo(message.from_user.id, help_img)
 
 
 async def _subscriptions(message) -> list[str]:
@@ -112,7 +107,7 @@ async def _subscriptions(message) -> list[str]:
 
 async def my_subscriptions(message):
 	if set(await _subscriptions(message)) == {0}:
-		await bot.send_message(message.from_user.id, 'Ты ещё не подписан ни на один хештег\U0001f625. Давай это исправим!')
+		await bot.send_message(message.from_user.id, 'Ты ещё не подписан ни на один хештег\u0001f625. Давай это исправим!')
 	await bot.send_message(message.from_user.id, 'Ты подписан на хештеги:')
 	await bot.send_message(message.from_user.id, '\n'.join(compress(HASHTAGS, await _subscriptions(message))))
 
@@ -120,17 +115,20 @@ async def my_subscriptions(message):
 @dp.message_handler(content_types=["text"])
 async def main(message):
 	global section
-	if section == 'main':
-		txt = message.text.strip()
-		if txt == 'Подписаться':
+	if section != 'main':
+		return
+
+	txt = message.text.strip()
+	match txt:
+		case 'Подписаться':
 			await subscribe(message)
-		elif txt == 'Отписаться':
+		case 'Отписаться':
 			await unsubscribe(message)
-		elif txt == 'Список подписок':
+		case 'Список подписок':
 			await my_subscriptions(message)
-		elif txt == 'Управление ботом':
-			await start(message)
-		elif txt == 'Наши контакты':
+		case "Помощь":
+			await send_help(message)
+		case "Контакты для связи":
 			await bot.send_message(message.from_user.id, CONTACTS_TEXT)
 
 	section = 'main'
